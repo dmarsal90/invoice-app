dc474478476e0dc27dfbd3a7e9edd043351a2995
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-12-09 18:31:04 -0500

    Update counter factory

--- database/factories/CounterFactory.php
+++ database/factories/CounterFactory.php
@@ -17,7 +17,9 @@ class CounterFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'key' => 'invoice',
+            'prefix' => 'INV-',
+            'value' => 20000
         ];
     }
 }
