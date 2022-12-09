0c105c004aa50de07bcc0c900506087dad3c566b
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-12-09 18:22:36 -0500

    Update counter migration

--- database/migrations/2022_12_09_231541_create_counters_table.php
+++ database/migrations/2022_12_09_231541_create_counters_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('counters', function (Blueprint $table) {
             $table->id();
+            $table->string('key')->unique();
+            $table->string('prefix');
+            $table->string('value');
             $table->timestamps();
         });
     }
